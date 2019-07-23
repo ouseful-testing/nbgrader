fdc33310200075edcedde9d6ac5a7d23d09139fe
Tony Hirst <tony.hirst@gmail.com>
2019-07-24 00:54:41 +0100

    Update nbgrader_config.py

--- nbgrader_config.py
+++ nbgrader_config.py
@@ -1,6 +1,7 @@
 c = get_config()
 
 # Add header and/or footer cells to a notebook.
-## Specify path to footer notebook, relative to the root of the course directory
+# Specify path to notebook, relative to the root of the course directory, 
+# that contains header / footer cells to be appended to generated assignment notebooks
 c.IncludeHeaderFooter.header = "source/boilerplate_header.ipynb"
 #c.IncludeHeaderFooter.footer = "source/boilerplate_footer.ipynb"
